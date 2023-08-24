6d255430e1fce8359e1c686a022a86c300c4d7de
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-08-24 12:58:25 -0400

    update

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
+        template: '.client/index.html',
         title: 'JATE'
       }),
 
